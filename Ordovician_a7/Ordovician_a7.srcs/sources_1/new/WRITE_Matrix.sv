1746671f808bd820dd147a1cb50789234371aae3
ZichaoZou <1076909409@qq.com>
2025-05-18 15:51:02 +0800

    更新WRITE模块

--- Ordovician_a7/Ordovician_a7.srcs/sources_1/new/WRITE_Matrix.sv
+++ Ordovician_a7/Ordovician_a7.srcs/sources_1/new/WRITE_Matrix.sv
@@ -1,23 +1,4 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 2025/05/13 20:21:54
-// Design Name: 
-// Module Name: WRTIE_Matrix
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
 
 module WRITE_Matrix (
     input logic clk,
